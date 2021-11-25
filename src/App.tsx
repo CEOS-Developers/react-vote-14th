97067e5e1806606b9777e620b76b0f01aaef87f6
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2021-11-25 20:19:05 +0900

    Routing set for required pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router";
+import Signup from "./components/signup/signup.components";
+import MainPage from "./components/mainpage/mainpage.components";
+import Login from "./components/login/login.components";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/signup/signup.components.tsx
@@ -0,0 +1,9 @@
+function Signup() {
+  return (
+    <>
+      <div>Signup</div>
+    </>
+  );
+}
+
+export default Signup;
