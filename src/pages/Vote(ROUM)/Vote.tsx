67c83e76e7f1053f3b873610e5aa87795f138bab
roum02 <roum02@ewhain.net>
2021-11-27 04:51:47 +0900

    vote layout

--- src/pages/Vote(ROUM)/Vote.tsx
+++ src/pages/Vote(ROUM)/Vote.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CssBaseline } from "@nextui-org/react";
+import { Radio } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+
+import { Logo, VoteBox, ButtonBox } from "./VotePresenter";
 
 const Vote = () => {
   const navigate = useNavigate();
   const handleLoginClick = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
-    <div>
-      투표창 (로움님 부탁드려요😁)
-      <div>
-        <div>
-          <button onClick={handleLoginClick}>로그인</button>
-        </div>
-      </div>
-    </div>
+    <>
+      <ButtonBox>
+        <Button onClick={handleLoginClick} auto color="primary" rounded flat>
+          로그인 하러가기
+        </Button>
+      </ButtonBox>
+      <Logo
+        alt=""
+        src="https://static.wixstatic.com/media/982853_0a088a0f99374ddd85634253179a8a43~mv2.jpeg/v1/fit/w_2500,h_1330,al_c/982853_0a088a0f99374ddd85634253179a8a43~mv2.jpeg"
+      />
+
+      <VoteBox>
+        <Radio.Group value="1">
+          <Radio value="1">
+            후보1
+            <Radio.Description>Description for Option1</Radio.Description>
+          </Radio>
+          <Radio value="2">
+            후보2<Radio.Desc>Description for Option2</Radio.Desc>
+          </Radio>
+          <Radio value="3">
+            후보3<Radio.Desc>Description for Option2</Radio.Desc>
+          </Radio>
+        </Radio.Group>
+      </VoteBox>
+    </>
   );
 };
 
---
+++ src/pages/Vote(ROUM)/VotePresenter.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Logo = styled.img`
+  width: 30rem;
+  display: block;
+  margin: 0 auto;
+`;
+
+export const VoteBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 45vh;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 1rem 2rem 0 0;
+`;
