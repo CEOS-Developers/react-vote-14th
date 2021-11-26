d997de6448b31cf40df0bbcff4d45d619f6aea79
kiuk <ugiya0628@icloud.com>
2021-11-27 01:22:57 +0900

    [DEV] Chore: RTK 초기세팅 및 디렉토리 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Provider store={store}>
+      <div className="App"></div>
+    </Provider>
   );
 }
 
