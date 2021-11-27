08dfdddf31d40c5ee0dc51b287f04c014f53758e
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-11-27 09:21:18 +0900

    backend, frontend voting page created with dummy data

---
+++ src/components/vote/frontend.vote.components.tsx
@@ -0,0 +1,12 @@
+import Candidates from "./candidates.vote.components";
+
+function Front() {
+  return (
+    <>
+      <h1 className="Title">CEOS 15기 프론트엔드 파트장 투표</h1>
+      <Candidates />
+    </>
+  );
+}
+
+export default Front;
