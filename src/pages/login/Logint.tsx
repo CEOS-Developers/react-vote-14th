628d700c63437322e9f134d9ee1c811863495887
kiuk <ugiya0628@icloud.com>
2021-11-27 11:09:50 +0900

    [DEV] Chore: 디렉토리 구조

---
+++ src/pages/login/Logint.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Logint = () => {
+  return <div></div>;
+};
+
+export default Logint;
---
+++ src/pages/vote/Vote.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Vote = () => {
+  return <div></div>;
+};
+
+export default Vote;
