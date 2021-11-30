2a7928d55ca7218114cd3aad604862889ef358bc
kiuk <ugiya0628@icloud.com>
2021-12-01 02:48:28 +0900

    [DEV] feat: custom axios 추가 (추후 토큰관련 헤더 포함하기

---
+++ src/app/customAxios.ts
@@ -0,0 +1,10 @@
+import { LOCAL_SERVER_URL } from '../config';
+import axios from 'axios';
+
+axios.defaults.baseURL = LOCAL_SERVER_URL;
+
+// axios.defaults.headers => for jwt
+
+axios.defaults.maxBodyLength = Infinity;
+axios.defaults.maxContentLength = Infinity;
+export default axios;
