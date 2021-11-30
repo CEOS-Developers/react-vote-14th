752af41a36046086efe5c692c8891c624f1d1049
kiuk <ugiya0628@icloud.com>
2021-12-01 02:47:37 +0900

    [WHAL-15] feat: auth 관련 hook, reducer 추가

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {
+  postLoginAsync,
+  LoginPayloadI,
+  postSignUpAsync,
+  SignUpPayloadI,
+} from '../app/auth/auth';
+import { useAppDispatch } from '../app/store';
 
 const useAuth = () => {
-  return <div></div>;
+  const dispatch = useAppDispatch();
+
+  const postLogin = (loginData: LoginPayloadI) => {
+    dispatch(postLoginAsync(loginData));
+  };
+
+  const postSignUp = (signUpData: SignUpPayloadI) => {
+    dispatch(postSignUpAsync(signUpData));
+  };
+
+  return { postLogin, postSignUp };
 };
 
 export default useAuth;
