6bba061fdeed88328366ba24ea21e70c38afdd62
Hyunjae Kim <peterhyunjae@naver.com>
2021-12-02 00:28:16 +0900

    📦 [build] : add axios

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+
 const SignIn = () => {
-  return <></>;
+  // 페이지 진입 시 id 입력 form에 focus.
+  // 각 input form 컴포넌트화해서 만들 것.
+
+  return (
+    <div>
+      <h1>Log in</h1>
+      <hr />
+      <p>15기 프론트/파트장 투표</p>
+      <form>
+        <input />
+        <input />
+        <button>Let's start!</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
