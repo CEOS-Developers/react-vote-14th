9a6cac32341b2cdac1410260acfa576a684a7e07
Hyunjae Kim <peterhyunjae@naver.com>
2021-12-02 02:15:15 +0900

    ✅ update test code

--- src/components/forms/LoginFormContainer.tsx
+++ src/components/forms/LoginFormContainer.tsx
@@ -1,5 +1,8 @@
 import useInputs from '../../hooks/useInput';
 import { FormContainer, InputContainer } from './LoginFormPresenter';
+// import { useLoadingContext } from '../../contexts/LoadingContext';
+import { Spinner } from '../Spinner';
+import axios from 'axios';
 
 // 수정 중
 // pw 안보이게 수정필요
@@ -9,11 +12,37 @@ const LoginFormContainer = () => {
 
   const onsubmit = (e: any) => {
     e.preventDefault();
-    console.log(userId, userPw);
+    // setLoading(true);
+    const form = new FormData();
+    form.append('username', userId);
+    form.append('password', userPw);
+
+    if (userId && userPw) {
+      console.log(userId, userPw);
+      axios
+        .post('https://vote-mailedit.kro.kr/api/signup', form)
+        .then((res) => {
+          // setloading 풀어줘
+          switch (res.status) {
+            case 200:
+              console.log(res.data);
+              console.log('login success');
+              break;
+            case 401:
+              console.log('일치하는 회원 없음');
+              break;
+          }
+        })
+        .catch((err) => {
+          //setLoading(false);
+          console.log(err);
+        });
+    }
   };
 
   return (
     <FormContainer onSubmit={onsubmit}>
+      <Spinner />
       <InputContainer
         type="text"
         className="IdForm"
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import LoginFormContainer from '../components/forms/LoginFormContainer';
 
 const SignIn = () => {
