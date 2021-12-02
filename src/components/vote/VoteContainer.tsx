5d3a579154ba6a9ba15bc28e96fd6a7f67a3ae94
Joohyun Kim <corinthionia@gmail.com>
2021-12-03 03:54:10 +0900

    :sparkles: feat: add useParams to redirect

--- src/components/vote/VoteContainer.tsx
+++ src/components/vote/VoteContainer.tsx
@@ -8,31 +8,56 @@ import {
 
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const VoteContainer = () => {
+  const { part } = useParams();
   const [candidates, setCandidates] = useState([]);
+  const [selectedCandidate, setSelectedCandidate] = useState('');
 
   useEffect(() => {
     const fetchCandidates = async () => {
       const response = await axios.get(
-        'https://9a63efda-a674-4015-be3c-824740a2aa52.mock.pstmn.io/vote/frontend'
+        `https://9a63efda-a674-4015-be3c-824740a2aa52.mock.pstmn.io/vote/${part}`
       );
       setCandidates(response.data);
     };
     fetchCandidates();
-  }, []);
+  }, [part]);
+
+  const handleCandidateButtonClick = (e: any) => {
+    setSelectedCandidate(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    axios
+      .post(
+        'https://9a63efda-a674-4015-be3c-824740a2aa52.mock.pstmn.io/selected',
+        selectedCandidate
+      )
+      .then((res) => {
+        console.log(res.config.data);
+        // console.log(selectedCandidate);
+
+        alert('Voted successfully');
+      });
+  };
 
   return (
     <Wrapper>
       <Title>Front-end</Title>
       <CandidatesWrapper>
         {candidates.map((candidate: any) => (
-          <CandidateButton key={candidate.id} value={candidate.name}>
+          <CandidateButton
+            key={candidate.name}
+            value={candidate.name}
+            onClick={handleCandidateButtonClick}
+          >
             {candidate.name}
           </CandidateButton>
         ))}
       </CandidatesWrapper>
-      <SubmitButton>Vote!</SubmitButton>
+      <SubmitButton onClick={handleSubmit}>Vote!</SubmitButton>
     </Wrapper>
   );
 };
