bba2c3b5b1b7f2e31573c9685c3e1ed1a0fdd529
bonjunku <ku96kbj@naver.com>
2021-12-02 13:08:36 +0900

    :sparkles: feat : http 모듈 구현

--- src/@shared/http/httpClient.ts
+++ src/@shared/http/httpClient.ts
@@ -3,4 +3,17 @@ export default class HttpClient {
   constructor(baseURL: string) {
     this.baseURL = baseURL;
   }
+
+  async fetch(url: string, options: any) {
+    const res = await fetch(`${this.baseURL}${url}`, {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...options.headers,
+      },
+    });
+    if (res.ok) {
+      return await res.json();
+    }
+  }
 }
