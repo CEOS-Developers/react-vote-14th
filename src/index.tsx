029b1dc3b9f543f70592bdedf1b22473aeac305c
bonjunku <ku96kbj@naver.com>
2021-12-02 19:56:47 +0900

    :lipstick: design : 앱 컨테이너 디자인

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,8 @@ import TokenStorage from './@shared/db/token';
 import AuthService from './@shared/service/auth';
 import VoteService from './@shared/service/vote';
 import Navbar from './@shared/components/navbar';
+import { CssBaseline } from '@nextui-org/react';
+import styled from 'styled-components';
 
 const baseURL = 'http://ec2-13-125-77-192.ap-northeast-2.compute.amazonaws.com';
 const tokenStorage = new TokenStorage();
@@ -17,18 +19,47 @@ const httpClient = new HttpClient(baseURL);
 const authService = new AuthService(httpClient, tokenStorage);
 const voteService = new VoteService(httpClient, tokenStorage);
 
+const Background = styled.div`
+  background: linear-gradient(
+      217deg,
+      rgba(231, 0, 200, 0.8),
+      rgba(255, 0, 0, 0) 70.71%
+    ),
+    linear-gradient(127deg, rgba(236, 169, 24, 0.8), rgba(0, 255, 0, 0) 70.71%),
+    linear-gradient(336deg, rgba(20, 55, 255, 0.8), rgba(0, 0, 255, 0) 70.71%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+const Container = styled.div`
+  background-color: #f3f3f3;
+  width: 75vmin;
+  height: 90vmin;
+  display: flex;
+  overflow: auto;
+  padding: 5vmin;
+  border-radius: 3vmin;
+`;
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/vote" element={<Vote />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </UserProvider>
-    </BrowserRouter>
+    <CssBaseline />
+    <Background>
+      <Container>
+        <BrowserRouter>
+          <UserProvider>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/vote" element={<Vote />} />
+              <Route path="/result" element={<Result />} />
+            </Routes>
+          </UserProvider>
+        </BrowserRouter>
+      </Container>
+    </Background>
   </React.StrictMode>,
   document.getElementById('root')
 );
