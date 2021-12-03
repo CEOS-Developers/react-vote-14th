907c5d495a5f0205651b4b34fccf3a5179c32cd0
Hyunjae Kim <peterhyunjae@naver.com>
2021-12-03 15:56:09 +0900

    ✨ [feat] : authcontext logic completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router';
 import { useLoadingContext } from './contexts/LoadingContext';
+import { useAuthContext } from './contexts/AuthContext';
+import axios from 'axios';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -7,9 +9,27 @@ import Vote from './pages/Vote';
 import Result from './pages/Result';
 
 import { Spinner } from './components/Spinner';
+import { useEffect } from 'react';
 
 function App() {
   const { loading }: any = useLoadingContext();
+  const { setIsAuth, setUserData }: any = useAuthContext();
+
+  // initialize fucntion
+  const initializeUserInfo = () => {
+    const ud = JSON.stringify(localStorage.getItem('userData'));
+    const token = JSON.stringify(localStorage.getItem('token'));
+    if (!ud) return;
+    setUserData(ud);
+    setIsAuth(true);
+    axios.defaults.headers.common['Authorization'] = token;
+  };
+
+  //initialize when first mount
+  useEffect(() => {
+    initializeUserInfo();
+  }, []);
+
   return (
     <>
       {loading ? <Spinner /> : null}
