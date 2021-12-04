bd7f154b03e139dcd5c7af3907221cd74c7eeb2d
kiuk <ugiya0628@icloud.com>
2021-12-04 16:23:43 +0900

    [WHAL-15] fix: server url 수정

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 exports.MOCK_SERVER_URL = 'http://localhost:4001/memo';
 exports.LOCAL_SERVER_URL = 'ttp://127.0.0.1:8000/';
+exports.SERVER_URL =
+  'http://ec2-15-164-222-123.ap-northeast-2.compute.amazonaws.com/';
