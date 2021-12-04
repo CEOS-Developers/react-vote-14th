9caa98352a0f4d572cb2dca8987b5afa40be858f
kiuk <ugiya0628@icloud.com>
2021-12-04 17:13:00 +0900

    [DEV] fix: api 전역 클래스로 변경

--- src/app/vote/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-export const requestGetCandidates = async () => {
-  const res = await axios.get('');
-  return res.data;
-};
-export const requestPostVote = async () => {
-  const res = await axios.post('');
-  return res.data;
-};
