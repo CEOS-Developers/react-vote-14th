489d94e5f008e2e358af15da24e0d1e86cd1ebc2
kiuk <ugiya0628@icloud.com>
2021-12-04 16:44:36 +0900

    [WHAL-15] chore: mock code 삭제

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import memoReducer from './ExampleRTK/exampleRTK';
 import { voteReducer } from './vote/reducer';
 
 const rootReducer = combineReducers({
-  memoReducer: memoReducer,
   voteReducer: voteReducer,
 });
 export default rootReducer;
