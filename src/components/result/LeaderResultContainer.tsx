7b938cc66709565548e7c13fa4d79f93135c4975
Joohyun Kim <corinthionia@gmail.com>
2021-12-26 02:23:15 +0900

    :dizzy: style: add confetti effect

--- src/components/result/LeaderResultContainer.tsx
+++ src/components/result/LeaderResultContainer.tsx
@@ -1,17 +1,26 @@
-import styled from 'styled-components';
-import COLORS from '../../constants/Colors';
+import Confetti from '../../utils/Confetti';
+import {
+  Box,
+  Container,
+  LeaderName,
+  Next,
+  PartName,
+  ResultWrapper,
+  Title,
+} from './LeaderResultPresenter';
 
 const LeaderResultContainer = () => {
   return (
     <Container>
+      <Confetti />
       <Title>축하합니다!</Title>
       <ResultWrapper>
         <Box>
-          <Part>프론트엔드 파트장</Part>
+          <PartName>프론트엔드 파트장</PartName>
           <LeaderName>🥇 김주현</LeaderName>
         </Box>
         <Box>
-          <Part>백엔드 파트장</Part>
+          <PartName>백엔드 파트장</PartName>
           <LeaderName>🥇 김주현</LeaderName>
         </Box>
       </ResultWrapper>
@@ -20,78 +29,4 @@ const LeaderResultContainer = () => {
   );
 };
 
-export const Container = styled.section`
-  width: 100vw;
-  height: 100vh;
-
-  display: grid;
-  justify-content: center;
-
-  grid-template-rows: 3fr 5fr 2fr;
-
-  grid-template-areas:
-    'title'
-    'result'
-    'next';
-  text-align: center;
-
-  color: ${COLORS.lightGray};
-  font-weight: 600;
-  background: ${COLORS.darkBackground};
-`;
-
-export const Title = styled.section`
-  grid-area: title;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  font-size: 36px;
-`;
-
-export const ResultWrapper = styled.section`
-  grid-area: result;
-
-  width: 500px;
-  height: 200px;
-
-  display: flex;
-  justify-content: center;
-
-  border: 1px solid ${COLORS.lightGray};
-  border-radius: 30px;
-`;
-
-export const Box = styled.section`
-  width: 50%;
-  height: 100%;
-`;
-
-export const Part = styled.h3`
-  height: 40%;
-  margin: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const LeaderName = styled.h4`
-  height: 40%;
-
-  margin: 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Next = styled.section`
-  grid-area: next;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 export default LeaderResultContainer;
--- src/components/result/LeaderResultPresenter.tsx
+++ src/components/result/LeaderResultPresenter.tsx
@@ -6,16 +6,17 @@ export const Container = styled.section`
   height: 100vh;
 
   display: grid;
+  justify-content: center;
 
-  grid-template-rows: 1.75fr 8.25fr;
+  grid-template-rows: 3fr 5fr 2fr;
 
   grid-template-areas:
     'title'
-    'result';
+    'result'
+    'next';
   text-align: center;
 
   color: ${COLORS.lightGray};
-  font-weight: 600;
   background: ${COLORS.darkBackground};
 `;
 
@@ -27,4 +28,49 @@ export const Title = styled.section`
   justify-content: center;
 
   font-size: 36px;
+`;
+
+export const ResultWrapper = styled.section`
+  grid-area: result;
+
+  width: 500px;
+  height: 200px;
+
+  display: flex;
+  justify-content: center;
+
+  border: 1px solid ${COLORS.lightGray};
+  border-radius: 30px;
+`;
+
+export const Box = styled.section`
+  width: 50%;
+  height: 100%;
+`;
+
+export const PartName = styled.h3`
+  height: 40%;
+  margin: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LeaderName = styled.h4`
+  height: 40%;
+
+  margin: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Next = styled.section`
+  grid-area: next;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
