341801537653a2a9a4a0e2395a0b541a25034ddb
Joohyun Kim <corinthionia@gmail.com>
2021-12-25 20:23:24 +0900

    :bug: fix: fix a bug

--- src/components/forms/LoginFormPresenter.tsx
+++ src/components/forms/LoginFormPresenter.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import COLORS from '../../constants/colors';
+import COLORS from '../../constants/Colors';
 
 export const FormContainer = styled.form`
   display: flex;
