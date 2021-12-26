079a80a8e2a167a0ebefa7f52d13ace135d83d32
TaeeunKim <xodms0309@naver.com>
2021-12-26 13:13:54 +0900

    fix: change api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 exports.MOCK_SERVER_URL = 'http://localhost:4001/memo';
 exports.LOCAL_SERVER_URL = 'ttp://127.0.0.1:8000/';
-exports.SERVER_URL =
-  'http://ec2-15-164-222-123.ap-northeast-2.compute.amazonaws.com/';
+exports.SERVER_URL = 'https://whaling-vote.kro.kr/';
